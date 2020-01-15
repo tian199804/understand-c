f95c2fed1d7e03bc07946215887e240dace801fb
tian199804 <59761618+tian199804@users.noreply.github.com>
2020-01-15 10:01:34 +0800

    Update summary.c

--- summary.c
+++ summary.c
@@ -11,7 +11,7 @@ int main(){
 	printf("a / b = %d\n", a / b);
 	printf("a %% b = %d\n", a % b);
 	printf("(a + b)/2 = %d\n",(a + b) / 2 );
-	printf("平均值 = %f\n\n", (double(a + b) / 2);
+	printf("平均值 = %f\n\n", (double)(a + b) / 2);
 		printf("半径:");
 		scanf("%lf", &r);
 		printf("半径为%.3f的圆面积是%.3f。\n",r,3.14 * r * r );
