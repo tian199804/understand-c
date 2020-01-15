ba98037e4c2da23fe737575de2abe4f9023505a9
tian199804 <59761618+tian199804@users.noreply.github.com>
2020-01-15 09:48:15 +0800

    Add files via upload

---
+++ chengfa.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void){
+	int n1;int n2;
+	int w;
+	printf("请输入两个整数。\n");
+	printf("整数1:\n");scanf ("%d",&n1);
+	printf("整数2:\n");scanf ("%d",&n2);
+	w =n1 * n2;
+	printf("它们的乘积是%d\n,w");
+	return 0;
+
+
+}
